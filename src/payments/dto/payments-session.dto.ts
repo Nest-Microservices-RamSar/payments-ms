e33e32ff66a0e502baa8bb6c973ba0c5a772fd96
ramiro <ramiro.sarasola@gmail.com>
2024-10-31 21:43:09 -0300

    create payments.dto and send new payments to stripe

---
+++ src/payments/dto/payments-session.dto.ts
@@ -0,0 +1,33 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNumber,
+  IsPositive,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class PaymentSessionDto {
+  @IsString()
+  public currency: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => PaymentSessionItemDto)
+  items: PaymentSessionItemDto[];
+}
+
+export class PaymentSessionItemDto {
+  @IsString()
+  public name: string;
+
+  @IsNumber()
+  @IsPositive()
+  public price: number;
+
+  @IsNumber()
+  @IsPositive()
+  public quantity: number;
+}
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payments-session.dto';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.STRIPE_SECRET);
 
-  async createPaymentSession() {
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items } = paymentSessionDto;
+
+    const lineItems = items.map((item) => ({
+      price_data: {
+        currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
+      quantity: item.quantity,
+    }));
+
     const session = await this.stripe.checkout.sessions.create({
       // Add here the order ID
       payment_intent_data: {
         metadata: {},
       },
 
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-Shirt',
-            },
-            unit_amount: 2000, // 20 dolares
-          },
-          quantity: 2,
-        },
-      ],
+      line_items: lineItems,
       mode: 'payment',
       // Redirecciones cuando se realizan los pagos.
       success_url: 'http://localhost:3000/payments/success',
